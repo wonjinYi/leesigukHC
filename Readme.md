5ef2e21a920110039773dc376c0dcbca2db0b046
wonjinYi <m_feeling@naver.com>
2020-05-24 11:13:48 +0000

    initial commit at goorm ide

---
+++ Readme.md
@@ -0,0 +1,60 @@
+# 라이센스와 적용범위
+license.md의 내용대로 BOB Please License를 적용합니다.
+적용 대상은 해당 repository에 있는 모든 리소스입니다.
+별도 협의없이 어떤 형태로든 변형해도 되고, 어떤 일이든 사용하셔도 됩니다.
+
+# 개발목적
+코로나19 대응에 따라 일부 보건소 민원업무의 일부 또는 전부가 중단되었습니다. 이에 따라 건강진단결과서(구 보건증)를 받고자 하는 사람은, 보건소에서 건강진단결과서 발급을 대행토록 한 민간 병/의원에서 검사를 받아야 합니다.
+
+여기에는 다음과 같은 문제가 있습니다.
+1. 민원인의 정보 부족으로 위치와 가격정보 비교 불가
+2. 보건소 및 관할 시군구청의 공지정보 자체의 파편화 및 양식/형태의 차이 발생
+3. 보건소 일부(또는 전부) 업무 중단사실 자체의 홍보부족
+
+코로나로 보건소 업무가 중단된 동안, 건강진단결과서를 필요로 하는 사람들에게 도움이 되고자 합니다.
+
+# 구축 데이터
+
+한계 : 유흥용이 아닌 일반용(일반요식업 및 단체급식용)만을 대상으로 조사하였습니다.
+유흥용 건강진단결과서에 대한 정보 얻기가 상당히 어려워 일단 보류하였습니다.
+
+2020년 5월 16일 현재 저는 두 세트의 데이터를 구축하였습니다. (서울)
+1. 서울 보건소 : 위치, 건강진단결과서 업무진행여부, 업무중단 판단근거
+> https://docs.google.com/spreadsheets/d/1G6WfRePThxwTO6hCypL2Jr2jxQoVSXbUVY7wqVp0iQE/edit?usp=sharing
+
+2. 서울 건강진단결과서 발급 가능 민간의료기관 현황 : 위치, 전화번호, 건강진단결과서 발급가격 등
+> https://docs.google.com/spreadsheets/d/1JRMu2YDemwclWB1cB7fPJmboVnEp2EmzcT6W2eoO4Gg/edit?usp=sharing
+
+두 데이터는 "기초 데이터"를 기반으로 정리, 수정, 가공 및 추가수집(검색, 전화문의, 온라인문의 등)과정을 거쳐 구축하였습니다. 별도 협의없이 어떤 형태로든 변형해도 되고, 어떤 일이든 사용하셔도 됩니다.
+
+# 기초 데이터
+1. 보건복지부 전국보건소 현황
+> http://www.mohw.go.kr/react/gm/sgm0601vw.jsp?PAR_MENU_ID=13&MENU_ID=1304020502&page=1&CONT_SEQ=293042
+
+2. 각 지역 관할 보건소의 '보건증 발급가능 민간의료기관 안내'
+> "구축 데이터"의 1번 "서울 보건소 현황"의 "근거"컬럼 등
+
+# 사용한 오픈소스프로젝트 / 서비스 / 기타 리소스
+* dataframe-js (MIT Lisence, Copyright (c) 2016 Guillaume Mousnier)
+> https://www.npmjs.com/package/dataframe-js 
+구글 스프레드 시트에서 추출된 csv를 지도작업에 알맞게 가공하는데 사용하였음
+
+* NAVER Maps API 
+> https://navermaps.github.io/maps.js.ncp/
+지도 표시에 사용하였음
+
+* Google SpreadsSheet 
+> https://www.google.com/intl/ko_kr/sheets/about/
+구축된 데이터를 웹서비스에서 사용하고 공유가 용이하도록 사실상의 DB로 활용하였음
+
+* 배달의 민족 주아 글꼴
+> http://font.woowahan.com/jua/
+로고에 사용한 폰트입니다
+
+* 나눔고딕 웹폰트
+> https://fonts.google.com/specimen/Nanum+Gothic
+페이지 전체에 사용한 폰트입니다.
+
+* 로고 및 마커 이미지
+> https://pixabay.com/images/id-1337908/
+로고에 마커+문서 이미지, 지도 마커에 마커 이미지를 사용하였습니다.
---
+++ script.js
@@ -0,0 +1,131 @@
+async function makeMarker(){
+  // csv파일 받아와 dictionary로 변환
+  var DataFrame = dfjs.DataFrame;
+  
+  let temp = [];
+  const HCC_SOURCE_URL = 'https://docs.google.com/spreadsheets/d/1G6WfRePThxwTO6hCypL2Jr2jxQoVSXbUVY7wqVp0iQE/export?format=csv';
+  const HOSPITAL_SOURCE_URL = 'https://docs.google.com/spreadsheets/d/1JRMu2YDemwclWB1cB7fPJmboVnEp2EmzcT6W2eoO4Gg/export?format=csv';
+  
+  await DataFrame.fromCSV(HCC_SOURCE_URL).then(df => {
+    temp[0] = df.toCollection();
+  });
+  await DataFrame.fromCSV(HOSPITAL_SOURCE_URL).then(df => {
+    temp[1] = df.toCollection();
+  });
+  const DATA = temp[0].concat(temp[1]);
+
+  // 마커 및 정보창 생성
+  let marker = [];
+  let infoWindow = [];
+ // let priceWindow = [];
+  let contentString = [];
+ // let priceString = [];
+
+  for(let i=0; i<Object.keys(DATA).length; i++){
+    //마커 생성
+    marker[i] = new naver.maps.Marker({
+      position: new naver.maps.LatLng(DATA[i]['Latitude'], DATA[i]['Longitude']),
+      map: map,
+      icon: {
+        url : "res/" + selectMarkerType(DATA[i]['일반용시행'], DATA[i]['기관유형']),
+        size: new naver.maps.Size(25, 39),
+        origin: new naver.maps.Point(0, 0),
+      }
+    });
+
+    //정보창 생성
+    //var infoMapURL = 'https://map.naver.com/v5/search/' + DATA[i]["기관명"];
+    var infoMapURL = 'https://m.map.naver.com/search2/search.nhn?query=' + DATA[i]["기관명"]+'&sm=hty&style=v5';
+    contentString = [
+      '<div style="padding:10px; text-align:left; color:#262626;">',
+      '   <a style="display:inline; text-decoration: none; font-size:20px; color:#262626;" href="'
+              +infoMapURL+'" target="_blank"> <strong>'+DATA[i]["기관명"]+' </strong> </a>',
+      '   <h3 style="display:inline; text-align:center; color:#EB5757;">'+isWorking(DATA[i]['일반용시행'])+'</h3><br />',
+      '   <hr color="#727272">',
+      '   <p style="margin:0;">',
+      '       전화번호 : '+DATA[i]['대표전화번호']+'<br />',
+      '       주소 : '+DATA[i]['주소']+'<br />',
+      '       가격 : '+isNull(DATA[i]['일반용가격']),
+      '   </p>',
+      '</div>'
+    ].join('');
+
+    infoWindow[i] = new naver.maps.InfoWindow({
+      content: contentString,
+      maxWidth:(document.getElementById('map').style.width - 10 ),
+      backgroundColor: "rgba(255,255,255,0.6)",
+      borderColor: "#727272",
+      borderWidth: 2,
+      anchorSize: new naver.maps.Size(0, 0),
+      pixelOffset: new naver.maps.Point(0, -10)
+    });
+
+    //가격표시창 생성
+    /*
+    priceString = [
+      '<div style="padding:10px; text-align:left; color:#262626;">',
+      '   <p style="margin:0;">',
+      '       '+isNull(DATA[i]['일반용가격']),
+      '   </p>',
+      '</div>'
+    ].join('');
+
+    priceWindow[i] = new naver.maps.InfoWindow({
+      content: priceString,
+      backgroundColor: "rgba(255,255,255,0.6)",
+      borderColor: "#727272",
+      borderWidth: 2,
+      anchorSize: new naver.maps.Size(0, 0),
+      pixelOffset: new naver.maps.Point(0, -10)
+    });
+    priceWindow[i].open(map, marker[i]);
+    */
+
+    naver.maps.Event.addListener(marker[i], "click", function(e) {
+      if (infoWindow[i].getMap()) {
+          infoWindow[i].close();
+          //priceWindow[i].open(map, marker[i]);
+      } else {
+          //priceWindow[i].close();
+          infoWindow[i].open(map, marker[i]);
+      }
+    });
+
+  }
+}
+
+function isNull(str){
+  if(str===''){return "정보가 없습니다";}
+  else{return str;}
+}
+
+function isWorking(isIssuing){
+  if(isIssuing=="FALSE"){return " < 발급업무 중단 >";}
+  else {return '';}
+}
+
+function selectMarkerType(isIssuing, type){
+  if(isIssuing=="FALSE"){return "marker_grey.png";}
+  else if(type=="보건소"){return "marker_blue.png";}
+  else if(type=="민간병원"){return "marker_green.png";}
+}
+
+// #######################################################################################################################
+// #######################################################################################################################
+// #######################################################################################################################
+
+// 네이버 맵 초기설정
+var mapOptions = {
+    center: new naver.maps.LatLng(37.550179, 127.073627),
+    zoom: 12
+};
+document.getElementById('map').clientHeight = "80%";
+
+var map = new naver.maps.Map('map', mapOptions);
+
+//마커 및 정보창 생성
+makeMarker();
+
+//지역선택창 만들기
+
+
