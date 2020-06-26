bfe16301cd843d3b766db03afe228094b15fbbd1
wonjin Yi <m_feeling@naver.com>
2020-06-26 08:23:18 +0000

    load latest notice by using json request

--- noticeScript.js
+++ noticeScript.js
@@ -1,18 +1,44 @@
 async function updateNotice(){
   	var DataFrame = dfjs.DataFrame;
-  
-	let temp = [];
-  	const NOTICE_URL = 'https://docs.google.com/spreadsheets/d/1LkhhtJj1M-6rQ2aBEsI6UXIu0ZVJ8AlDS2yDNTfAvfQ/export?format=csv';
-  
-	await DataFrame.fromCSV(NOTICE_URL).then(df => {
-	temp = df.toCollection();
-	});
 	
+	//for assigning ONLY THE latest notice
+	let notice;
 	
-	const LATEST_NOTICE = temp[0]["공지사항"];
-	document.getElementById('notice').innerHTML = "<b>"+LATEST_NOTICE+"</b>";
+	//for assigning every notices
+	//let notice = [];
+	
+
+	const NOTICE_URL = 'https://spreadsheets.google.com/feeds/list/1LkhhtJj1M-6rQ2aBEsI6UXIu0ZVJ8AlDS2yDNTfAvfQ/1/public/full?alt=json';
+  	console.log("AAAAAA");
+	
+	var request = new XMLHttpRequest();
+	request.open('GET', NOTICE_URL);
+	request.responseType = 'json';
+	request.send();
+	request.onload = function() {
+		
+		//////////////////////////////////////
+		//for assigning ONLY THE latest notice
+		var temp = request.response;
+		notice = temp['feed']['entry'][0].gsx$공지사항.$t;
+		//console.log(notice);
+		document.getElementById('notice').innerHTML = "<b>"+notice+"</b>";
+		//////////////////////////////////////
+		
+		
+		/////////////////////////////
+		//for assigning every notices 
+		/*
+		temp = temp['feed']['entry'];
+		for(var i=0; i<Object.keys(temp).length; i++){
+			notice[i] = temp[i].gsx$공지사항.$t
+		}
+		console.log(notice);
+		*/
+		/////////////////////////////
+		
+	}
 }
 
 //마커 및 정보창 생성
 window.addEventListener('DOMContentLoaded', updateNotice);
-
